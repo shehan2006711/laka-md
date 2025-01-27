3128cfaf668089584b68170669342bd6654113b4
shehan2006711 <154323632+shehan2006711@users.noreply.github.com>
2025-01-27 16:55:10 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ module.exports = {
 //👉 Put Your Session Id 🐣 👈කියන එක කපලා අයින් කරලා ඔයාගේ Session id එක දාන්න.
 SESSION_ID: process.env.SESSION_ID || "LAKA-MD=pwwkTa4D#ynWlNRVzQiwFhuJotdQbZtLtRvuUXkTIRfvh8NLrW5E",
 //👉 Put Your Mongode Url 🖇️ 👈කියන එක කපලා අයින් කරලා ඔයාගේ Mongode Url එක දාන්න.
-MONGODB: process.env.MONGODB || "mongodb+srv://shehan94:shehan123@cluster0.nhx53dt.mongodb.net/",
+MONGODB: process.env.MONGODB || "mongodb://mongo:dHFG-ef6CAFaGf5F1Gf-Cbg6AadbDcff@viaduct.proxy.rlwy.net:11588",
 PREFIX: process.env.PREFIX || '.' ,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94781121579' : process.env.OWNER_NUMBER,
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
